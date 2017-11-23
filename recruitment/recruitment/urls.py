68771b970907bb35fbe96d32311484342dad6357
asusai <asusai@paypalcorp.com>
2017-11-23 12:20:41 +0530

    url changes to recent version

--- recruitment/recruitment/urls.py
+++ recruitment/recruitment/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+import application.views as application_view
 
 urlpatterns = [
     # Examples:
@@ -7,13 +8,13 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),url(r'^dashboard/', 'application.views.dashboard'),
 
     #url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'application.views.application_form'),
-    url(r'^admin/', 'application.views.admin'),
-    url(r'^dashboard/', 'application.views.dashboard'),
-    url(r'^users/(?P<status>[\w-]+)/$', 'application.views.users'),
-    url(r'^user/(?P<user_id>[\d]+)/$', 'application.views.user'),
-    url(r'^update_status/$', 'application.views.update_status'),
-    url(r'^logout/$', 'application.views.logout'),
+    url(r'^$', application_view.application_form, name='application'),
+    url(r'^admin/', application_view.admin, name='admin'),
+    url(r'^dashboard/', application_view.dashboard, name='dashboard'),
+    url(r'^users/(?P<status>[\w-]+)/$', application_view.users, name='users'),
+    url(r'^user/(?P<user_id>[\d]+)/$', application_view.user, name='user'),
+    url(r'^update_status/$', application_view.update_status, name='update_status'),
+    url(r'^logout/$', application_view.logout, name='logout'),
 
 ]
 
